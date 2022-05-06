a9314fa02fe4a7c5fd052d2003d7c5adc4a6adeb
Selam <sehabte@miu.edu>
2022-05-06 12:55:54 -0500

    w2d4

--- w2d1/bank.js
+++ w2d1/bank.js
@@ -42,5 +42,17 @@ function createAccount() {
 
   document.getElementById('output').value = output;
 
+var x = 10;
+function main(){
+  console.log(x);
+  x =20;
+  if(x>0){
+    var x = 30;
+    console.log(x);
+  }
+  var x =40;
+  var f = function(x) {console.log(x);}
+  f(50);
 
+}
 }
